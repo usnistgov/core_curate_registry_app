dbc64f93043ec4ceba416d906158a7ca74b97c8d
Xavier SCHMITT <xavier.schmitt@nist.gov>
2018-06-27 16:37:22 -0400

    feat: add api to get role from form
    Change-Id: If504ac60b0ddab8182a88b358cf2ac0d7c63f8b9

---
+++ core_curate_registry_app/components/curate_data_structure/api.py
@@ -0,0 +1,17 @@
+""" Curate Data Structure registry api
+"""
+from core_main_app.utils import xml as xml_utils
+
+from core_main_registry_app.utils.role.extraction import role_extraction
+
+
+def get_role(curate_data_structure):
+    """ Get the role saved in the curate_data_structure's form string
+
+    Args:
+        curate_data_structure:
+
+    Returns:
+
+    """
+    return role_extraction(xml_utils.raw_xml_to_dict(curate_data_structure.form_string, xml_utils.post_processor))
