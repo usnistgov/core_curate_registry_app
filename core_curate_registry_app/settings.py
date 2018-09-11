d9bfa5c033dd7c3a5180d8762923430a27443463
Xavier SCHMITT <xavier.schmitt@nist.gov>
2018-09-11 11:39:31 -0400

    feat: add i18n + fix draft label
    Change-Id: Id17bdf4269d4d67acade28dafbc1e2f221319065

--- core_curate_registry_app/settings.py
+++ core_curate_registry_app/settings.py
@@ -1,3 +1,6 @@
+"""Core Curate App Settings
+"""
+import os
 from django.conf import settings
 
 if not settings.configured:
@@ -6,3 +9,9 @@ if not settings.configured:
 REGISTRY_XSD_FILENAME = getattr(settings, 'REGISTRY_XSD_FILENAME', "")
 """ str: Registry xsd filename used for the initialisation.
 """
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'core_curate_registry_app/locale'),
+)
