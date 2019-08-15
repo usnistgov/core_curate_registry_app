17b06ffb215ed0221ccbcd4293c54cb466653d57
and15 <ali.daoudi@nist.gov>
2019-08-15 11:48:01 -0400

    fix: role display
    Change-Id: Id3fee27683ad7ca7b55256632ccedc5698840053

--- core_curate_registry_app/static/core_curate_registry_app/user/js/start_curate.js
+++ core_curate_registry_app/static/core_curate_registry_app/user/js/start_curate.js
@@ -1,4 +1,28 @@
 $(document).ready(function(){
     load_start_form($("#template_id").html());
     $('#btn-display-data').on('click', displayTemplateProcess);
+        var MAX_INTERVAL_ITER = 5000;
+    var iteration = 0;
+
+    var interval = setInterval(function() {
+        iteration++;
+        if (iteration >= MAX_INTERVAL_ITER) clearInterval(interval);
+        if ($("#form_start_content").text().length> 1) {
+            clearInterval(interval);
+            displayRoleTitle();
+            }
+    }, 100);
+
 });
+
+var displayRoleTitle = function() {
+if ($("#role_title").text() != "")
+    {
+        role_title = "Create a new " + $("#role_title").text() + ":";
+        $("#form_role_title").text(role_title);
+    }
+else
+    {
+        $("#form_role_title").text("Create a new resource :");
+    }
+}
