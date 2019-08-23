96553539f0832c86edce3f56907d1907e1195eac
Augustin CHINI <augustin.chini@nist.gov>
2019-08-23 13:55:02 -0400

    fix: flex resource banner
    Change-Id: I25fac01745fb6fb4376c089975266aab999ca4f0

--- core_curate_registry_app/static/core_curate_registry_app/user/js/banner.js
+++ core_curate_registry_app/static/core_curate_registry_app/user/js/banner.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-    $("#icons_table td").each(function(){
+    $("#icons_banner div").each(function(){
         $(this).on("click", action_click);
     }) ;
 });
 
 var action_click = function(event){
-    var $td = $(event.target).closest('td');
-    var slug = $td.attr("id").replace('td_','');
+    var $td = $(event.target).closest('div');
+    var slug = $td.attr("id").replace('cnt_','');
     var link_id = slug+"_link"
     window.location = $("#" + link_id).attr("href");
 }
