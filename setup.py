2ec99d80e171e2264c01ccf633c645a0232747d1
Guillaume Sousa <guillaume.sousa@nist.gov>
2023-09-25 16:58:18 +0000

    chore: upgrade 2.6.0
    Change-Id: Ifd7acebcc04758ba6f09417b37d27a17b64dd6a0

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ required = [req_link(r) if r.startswith("https://") else r for r in required]
 
 setup(
     name="core_curate_registry_app",
-    version="2.5.0",
+    version="2.6.0",
     description="Curation functionalities specific to Registry applications for the curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
